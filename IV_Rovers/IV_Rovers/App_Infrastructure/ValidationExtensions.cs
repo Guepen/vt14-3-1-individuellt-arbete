6859099740f14ce544c5991e2266703ae3a15af4
th222fa <th222fa@student.lnu.se>
2014-03-10 23:47:03 +0100

    ValidationExtension class created

---
+++ IV_Rovers/IV_Rovers/App_Infrastructure/ValidationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using IV_Rovers.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IV_Rovers
+{
+    public static class ValidationExtensions
+    {
+        public static bool Validate(this Player instance, out ICollection<ValidationResult> validationResults)
+        {
+            var validationContext = new ValidationContext(instance);
+            validationResults = new List<ValidationResult>();
+            return Validator.TryValidateObject(instance, validationContext, validationResults, true);
+        }
+             
+    }
+}
